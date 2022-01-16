c2cd58e9efbe6fe1d97234314ab3a40b7502b993
Jayen <jayen@jayenashar.org>
2022-01-16 21:57:46 +1100

    exported build with new example sharing state between components of different types

--- _next/static/MAycZSdSSvVcLdHna9AIo/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b282b959608f58d4.css","static/chunks/pages/index-c90e3de3338129b0.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/lKggrUQQEYMYoD7V3QVRk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c143dd11760718d1.css","static/chunks/pages/index-95db90ae3c81f5f8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
